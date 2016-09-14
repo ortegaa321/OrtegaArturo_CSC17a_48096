98e999e30842f7682d8b7b51eadffd75689f995d
rcc <rcc>
2016-09-14 14:27:45 -0700

    Signed-off-by: rcc <rcc>

---
+++ Class/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on September 12, 2016, 1:01 PM
+ * Purpose:  Simple template for all your programs
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begin Here!
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Input data
+    
+    //Process -> Map the input data to the output
+    
+    //Display/Output solution here
+
+    //Exit stage right!
+    return 0;
+}
